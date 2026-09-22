7ca631dff7947defb1ad90cdd542c49e1581f9aa
javierpm26 <javierparra909@gmail.com>
2026-09-22 15:24:42 +0200

    Apartado 7 - Crear los objetos completado

--- Actividad_POO_Plataforma_Streaming/Plataforma-Streaming/Program.cs
+++ Actividad_POO_Plataforma_Streaming/Plataforma-Streaming/Program.cs
@@ -1,9 +1,48 @@
-﻿namespace PlataformaStreaming
+﻿using Plataforma_Streaming.Entidades;
+
+namespace PlataformaStreaming
 {
     class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
+            Cancion cancion1 = new Cancion(
+                "Faded",
+                330,
+                0,
+                "Alan Walker",
+                "EDM",
+                false);
+
+            Cancion cancion2 = new Cancion(
+                "Alone",
+                416,
+                0,
+                "Marshmello",
+                "EDM",
+                false);
+
+            Podcast podcast1 = new Podcast(
+                "The Wild Project",
+                120,
+                0,
+                "Jordi Wild",
+                25);
+
+            Podcast podcast2 = new Podcast(
+                "Desayuno Royale",
+                50,
+                0,
+                "Alvaro 845",
+                129);
+
+            Audiolibro audiolibro = new Audiolibro(
+                "El misterio de la ciudad",
+                240,
+                0,
+                "Eva García",
+                "Juan Magraner",
+                true);
 
         }
 
