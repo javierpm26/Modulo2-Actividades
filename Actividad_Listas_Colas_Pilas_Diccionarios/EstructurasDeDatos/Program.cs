07247d2c5a55d4595ff96a87b66ec73ffc77037a
javierpm26 <javierparra909@gmail.com>
2026-09-23 15:19:37 +0200

    Estructura de archivos creada en Program.cs creada la estructura inicial de clases

--- Actividad_Listas_Colas_Pilas_Diccionarios/EstructurasDeDatos/Program.cs
+++ Actividad_Listas_Colas_Pilas_Diccionarios/EstructurasDeDatos/Program.cs
@@ -1,1 +1,10 @@
-﻿Console.WriteLine("Hello, World!");
+﻿namespace EstructuraDeDatos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"Hola mundo");
+        }
+    }
+}
