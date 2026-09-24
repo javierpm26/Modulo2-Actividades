b9a8913722df594ecdca40782ca61461e97ddeea
javierpm26 <javierparra909@gmail.com>
2026-09-24 17:43:04 +0200

    Ejercicio 11 - Agenda de Contactos completado

--- Actividad_Listas_Colas_Pilas_Diccionarios/EstructurasDeDatos/Utilities/Diccionarios.cs
+++ Actividad_Listas_Colas_Pilas_Diccionarios/EstructurasDeDatos/Utilities/Diccionarios.cs
@@ -108,6 +108,7 @@ namespace EstructurasDeDatos.Utilities
             }
         }
 
+        // Ejercicio 11 - Agenda de contactos
         public static void AgendaContactos()
         {
             Dictionary<string, int> contactos = new();
@@ -122,7 +123,7 @@ namespace EstructurasDeDatos.Utilities
                 Console.WriteLine("1. Añadir contacto\n2. Buscar teléfono\n3. Modificar teléfono\n4. Eliminar contacto\n5. Mostrar contactos\n6. Salir");
 
                 // escaner
-                Console.WriteLine("Tu opción: ");
+                Console.Write("Tu opción: ");
                 opcionUsuario = int.Parse(Console.ReadLine() ?? "");
 
                 switch(opcionUsuario)
@@ -137,16 +138,47 @@ namespace EstructurasDeDatos.Utilities
                     
                     case 2: 
                         Console.WriteLine("Escribe el nombre del contacto que deseas buscar: ");
-                        string buscarContacto = Console.ReadLine() ?? "";
-                        contactos.TryGetValue(buscarContacto, out int contactoBuscado);
-                        Console.WriteLine($"Resultado de la búsqueda: ({buscarContacto})");
+                        string contactoBuscar = Console.ReadLine() ?? "";
+                        contactos.TryGetValue(contactoBuscar, out int numeroTelefono);
+                        Console.WriteLine($"El número de teléfono de {contactoBuscar} es: {numeroTelefono}");
                         break;
                     
-                    //case 3: 
-                        //Console.WriteLine("Escribe el número de teléfono que deseas cambiar: ");
-                        //int modificarTelefono = int.Parse(Console.ReadLine() ?? "");
+                    case 3: 
+                        Console.WriteLine("Nombre del contacto a cambiar el número de teléfono: ");
+                        string contactoModificar = Console.ReadLine() ?? "";
+                        Console.WriteLine("Escribe el nuevo número de teléfono: ");
+                        int nuevoNumero = int.Parse(Console.ReadLine() ?? "");
+                        contactos[contactoModificar] = nuevoNumero;
+                        Console.WriteLine($"Contacto modificado: {contactoModificar} - Nuevo número: {nuevoNumero}");
+                        break;
 
-                        //contactos[modificarTelefono] = 
+                    case 4: 
+                        Console.WriteLine("Escribe el nombre del contacto que deseas eliminar: ");
+                        string contactoEliminar = Console.ReadLine() ?? "";
+                        contactos.Remove(contactoEliminar);
+                        Console.WriteLine($"Contacto eliminado: {contactoEliminar}");
+                        break;
+                    
+                    case 5:
+                        Console.WriteLine("=== MIS CONTACTOS === ");
+                        foreach(KeyValuePair<string, int> contacto in contactos)
+                        {
+                            Console.WriteLine($"Nombre: {contacto.Key}, Teléfono: {contacto.Value}");
+                        }
+                        break;
+
+                    case 6:
+                        Console.WriteLine("Saliendo del programa...");
+                        break;
+                    
+                    default:
+                        Console.WriteLine("¡¡Opción inválida!!");
+                        break;
+
+                    
+
+                    
+
 
                         
                         
